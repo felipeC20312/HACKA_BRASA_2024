94050a6fba3c67a47b3009a047dca6a21f0740f9
felipeC20312 <felipecaetanodev@gmail.com>
2024-08-24 16:37:09 -0300

    felipeC20312
    Develop - Sidebar + BreadCrumb + Pages

---
+++ src/components/breadcrumb/index.tsx
@@ -0,0 +1,28 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+
+export default function BreadcrumbCustom() {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/'>Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href='pages/Dashboard'>Components</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import BreadcrumbCustom from '../breadcrumb';
+
+export default function SideBar({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='flex'>
+      <div className='flex flex-col w-auto h-screen p-6 items-center gap-8 bg-[#09090B] border-r border-[#27272A]'>
+        <Link href='/'>
+          <button className='size-10 p-4 rounded-full bg-blue-500 text-white'></button>
+        </Link>
+        <Link href='/pages/dashboard'>
+          <button className='size-10 p-4 rounded-full bg-blue-500 text-white'></button>
+        </Link>
+        <Link href='pages/'>
+          <button className='size-10 p-4 rounded-full bg-blue-500 text-white'></button>
+        </Link>
+      </div>
+
+      <div className='w-full'>
+        <div className='flex w-full h-20 bg-[#151518] text-white items-center border-b border-solid border-[#27272A]'>
+          <BreadcrumbCustom />
+        </div>
+        <div className='flex w-full h-20 bg-[#151518] text-white items-center border-b border-solid border-[#27272A]'>
+          Teste
+        </div>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+}
